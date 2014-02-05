7cc59cd238a7d19c685033850d3e5c855e381593
David Minnerly <dtreeLive@hotmail.com>
2014-02-05 13:27:21 -0500

    Fix .length issue going above the array size.
    Add js/ to JSON file url.

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	 * The main logic, gets the data.json file and
 	 * sets a Quote and Background.
 	 */
-	$.getJSON('data.json', function(data) {
+	$.getJSON('js/data.json', function(data) {
 		getRandomQuote(data);
 		getRandomBackground(data);
 	});
@@ -31,7 +31,10 @@ $(document).ready(function() {
 	 * image from the backgrounds array to display.
 	 */
 	function getRandomBackground(data) {
-		var i = randomInt(0, data.backgrounds.length);
+		// .length starts counting at 1 instead of 0, so 1 needs
+		// to be subtracted to get correct array length.
+		var i = randomInt(0, data.backgrounds.length - 1);
+
 		setBackgroundImage(data.backgrounds[i].url)
 	}
 
@@ -45,7 +48,10 @@ $(document).ready(function() {
 	 * link from data.json and display them on screen.
 	 */
 	function getRandomQuote(data) {
-		var i = randomInt(0, data.quotes.length);
+		// .length starts counting at 1 instead of 0, so 1 needs
+		// to be subtracted to get correct array length.
+		var i = randomInt(0, data.quotes.length - 1);
+
 		quote_p.html(data.quotes[i].one.quote);
 		quote_cite.html(data.quotes[i].author);
 
