ae54b01d4f2df0c90c4486c4f71242d73ffa99b4
David Minnerly <dtreeLive@hotmail.com>
2014-02-04 21:00:01 -0500

    Rename existing functions.

--- js/app.js
+++ js/app.js
@@ -13,9 +13,10 @@ $(document).ready(function() {
 	 * sets a Quote and Background.
 	 */
 	$.getJSON('data.json', function(data) {
-		quote(data);
-		background(data);
+		getRandomQuote(data);
+		getRandomBackground(data);
 	});
+
 
 	/**
 	 * Generate a random integer, primarily used for getting
@@ -25,24 +26,26 @@ $(document).ready(function() {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
+
 	/**
 	 * Hooks into the data.json file and gets a random
 	 * image from the backgrounds array to display.
 	 */
-	function background(data) {
+	function getRandomBackground(data) {
 		var i = randomInt(0, data.backgrounds.length);
-		backgroundImage(data.backgrounds[i].url)
+		setBackgroundImage(data.backgrounds[i].url)
 	}
 
-	function backgroundImage(image) {
+	function setBackgroundImage(image) {
 		return $('body').css('background-image', 'url(img/' + image + ')');
 	}
+
 
 	/**
 	 * Get a random Quote with corresponding Citation and source
 	 * link from data.json and display them on screen.
 	 */
-	function quote(data) {
+	function getRandomQuote(data) {
 		var i = randomInt(0, data.quotes.length);
 		quote_p.html(data.quotes[i]._001.quote);
 		quote_cite.html(data.quotes[i].author);
@@ -52,4 +55,3 @@ $(document).ready(function() {
 		// }
 	}
 });
-
