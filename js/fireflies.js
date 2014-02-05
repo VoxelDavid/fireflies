e353258cfd600c4c4de035c603db92f99f65ce3d
David Minnerly <dtreeLive@hotmail.com>
2014-02-04 19:23:13 -0500

    Set comment blocks for each function.

--- js/fireflies.js
+++ js/fireflies.js
@@ -5,22 +5,41 @@ $(document).ready(function() {
 	    quote_p    = $('.quote p'),
 	    quote_cite = $('.quote cite');
 
-	// Random integers used to decide which Quote and Background to use.
+	/**
+	 * The main logic, gets the fireflies.json file and
+	 * sets a Quote and Background.
+	 */
+	$.getJSON('fireflies.json', function(data) {
+		quote(data);
+		background(data);
+		$('body').css('background-image', 'url(img/bg-highway.jpg)');
+	});
+
+	/**
+	 * Generate a random integer, primarily used for getting
+	 * an Array index.
+	 */
 	function randomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
-	// Setting the background
+	/**
+	 * Hooks into the fireflies.json file and gets a random
+	 * image from the backgrounds array to display.
+	 */
 	function background(data) {
 		var i = randomInt(0, data.backgrounds.length);
-		setBackground(data.backgrounds[i].url)
+		backgroundImage(data.backgrounds[i].url)
 	}
 
-	function setBackground(image) {
+	function backgroundImage(image) {
 		return $('body').css('background-image', 'url(img/' + image + ')');
 	}
 
-	// Setting Quotes, citations and source links.
+	/**
+	 * Get a random Quote with corresponding Citation and source
+	 * link from fireflies.json and display them on screen.
+	 */
 	function quote(data) {
 		var i = randomInt(0, data.quotes.length);
 		quote_p.html(data.quotes[i]._001.quote);
@@ -30,10 +49,5 @@ $(document).ready(function() {
 		// 	console.log("Rabbits");
 		// }
 	}
-
-	$.getJSON('fireflies.json', function(data) {
-		quote(data);
-		background(data);
-	});
 });
 
