fb9f2d23e63b3275cba76c4699ef7d382c559c6c
David Minnerly <dtreeLive@hotmail.com>
2014-02-05 21:22:53 -0500

    Fix linting errors, add function comment blocks.

--- js/app.js
+++ js/app.js
@@ -4,36 +4,35 @@ $(document).ready(function() {
 
 	var quote_element = $('.quote');
 
-	/**
-	 * The main logic, gets the data.json file and
-	 * sets a Quote and Background.
-	 */
+
+	// The main logic, gets the data.json file and
+	// sets a Quote and Background.
+
 	$.getJSON('js/data.json', function(data) {
 		getRandomQuote(data);
 		getRandomBackground(data);
 	});
 
 
-	/**
-	 * Generate a random integer, primarily used for getting
-	 * an Array index.
-	 */
+
+	// Generate a random integer, primarily used for getting
+	// an Array index.
+
 	function randomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
 
-	/**
-	 * Hooks into the data.json file and gets a random
-	 * image from the backgrounds array to display.
-	 */
+	// Hooks into the data.json file and gets a random
+	// image from the backgrounds array to display.
+
 	function getRandomBackground(data) {
 		// .length starts counting at 1 instead of 0, so 1 needs
 		// to be subtracted to get correct array length.
-		var i = randomInt(0, data.backgrounds.length - 1);
-		var chosen_bg = data.backgrounds[i];
+		var i = randomInt(0, data.backgrounds.length - 1),
+			chosen_bg = data.backgrounds[i];
 
-		setBackgroundImage(chosen_bg.url)
+		setBackgroundImage(chosen_bg.url);
 		setTextColor(chosen_bg.style);
 	}
 
@@ -41,6 +40,9 @@ $(document).ready(function() {
 		$('body').css('background-image', 'url(img/' + image + ')');
 	}
 
+
+	// param (string) bg_style - The style of background. Specified in data.json
+	//                           for each background image.
 
 	function setTextColor(bg_style) {
 		// Choose which color text to display depending on
@@ -53,22 +55,28 @@ $(document).ready(function() {
 	}
 
 
+	// Generate two random numbers to select a quote from the json file.
+	// ================================
+	// param (string) data - Quotes and background data from 'data.json'.
 
-	/**
-	 * Get a random Quote with corresponding Citation and source
-	 * link from data.json and display them on screen.
-	 */
 	function getRandomQuote(data) {
 		// .length starts counting at 1 instead of 0, so 1 needs
 		// to be subtracted to get correct array length.
 		var i = randomInt(0, data.quotes.length -1),
-		    quote_root = data.quotes[i];
+			quote_root = data.quotes[i];
 
 		var x = randomInt(0, quote_root.quote_list.length -1),
-		    chosen_quote = quote_root.quote_list[x];
+			chosen_quote = quote_root.quote_list[x];
 
 		generateQuoteMarkup(quote_root, chosen_quote);
 	}
+
+
+	// Creates the required markup for the Quotes
+	// ================================
+	// param (object) quote_root   - The first level in the 'quotes' array, where
+	//                               the author/title values are stored.
+	// param (object) chosen_quote - The quote chosen under quote_root.quote_list.
 
 	function generateQuoteMarkup(quote_root, chosen_quote) {
 
@@ -89,7 +97,7 @@ $(document).ready(function() {
 
 			if (quote_root.title) {
 				$('.quote cite a')
-					.attr('title', quote_root.title)
+					.attr('title', quote_root.title);
 			}
 		} else if (quote_root.author) {
 			$('.quote cite')
