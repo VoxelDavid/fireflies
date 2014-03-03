0ae97aa5d92a880a920c4d405efd70bb6e0e3d5f
David Minnerly <dtreeLive@hotmail.com>
2014-03-03 15:07:51 -0500

    Remove grunt-bower-install.
    It only worked for the file set as 'main' in the bower.json file,
    making it impossible to use .min variants without editing that
    property. And it would likely get overwritten on a package update.

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,10 +44,6 @@ module.exports = function(grunt) {
 				files: ['<%= app.jsDir %>/{,*/}*.js'],
 				tasks: ['newer:jshint:lint']
 			},
-			bower: {
-				files: ['<%= app.bowerDir %>/{,*/}*'],
-				tasks: ['bowerInstall']
-			},
 			livereload: {
 				files: [
 					'<%= app.base %>/{,*/}*.html',
@@ -69,26 +65,11 @@ module.exports = function(grunt) {
 			lint: [
 				'<%= app.base %>/{,*/}*.js'
 			]
-		},
-
-
-		// Build Tasks
-
-		// Automatically inject tags for installed Bower packages.
-		bowerInstall: {
-			// Options reference:
-			// https://github.com/stephenplusplus/grunt-bower-install#getting-started
-
-			app: {
-				src: ['<%= app.base %>/index.html'],
-				ignorePath: '<%= app.base %>/'
-			}
 		}
 
 	});
 
 	grunt.registerTask('serve', [
-		'bowerInstall',
 		'connect',
 		'watch',
 		'jshint'
