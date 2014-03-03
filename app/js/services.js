08ffb28e42ef4452a2b4785a6e5e526aede1bc45
David Minnerly <dtreeLive@hotmail.com>
2014-03-03 17:07:27 -0500

    Add services.js.

---
+++ app/js/services.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+/* Services */
+
+app.factory('Quote', function() {
+	var Quote = {
+
+	};
+
+	return Quote;
+});
+
+app.factory('Photo', function() {
+	var Photo = {
+
+	};
+
+	return Photo;
+});
+
