9caa8768ba096b3de822b27a4aafd981ff08fa7c
David Minnerly <dtreeLive@hotmail.com>
2014-03-03 17:02:04 -0500

    Create an external js file for controllers.

--- app/js/app.js
+++ app/js/app.js
@@ -5,15 +5,10 @@ var app = angular.module('firefliesApp', [
 	'ngRoute'
 ]);
 
-app.controller('MainCtrl', function($scope) {
-	$scope.quote = 'Hello, World!';
-	$scope.author = 'Brian Kernighan';
-});
-
 app.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'views/index.html',
-			controller: 'MainCtrl'
-		})
+			controller: 'FirefliesCtrl'
+		});
 });
---
+++ app/js/controllers.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+/* Controllers */
+
+app.controller('FirefliesCtrl', function($scope) {
+	$scope.quote = 'Hello, World!';
+	$scope.author = 'Brian Kernighan';
+});
