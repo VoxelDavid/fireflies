f0932b9f9e6724cb86410dc7bdaf5a5771f8d0e9
David Minnerly <dtreeLive@hotmail.com>
2014-03-03 15:50:37 -0500

    Setup some basic routing for the index file.

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,19 @@
 
 'use strict';
 
-var app = angular.module('firefliesApp', []);
+var app = angular.module('firefliesApp', [
+	'ngRoute'
+]);
 
 app.controller('MainCtrl', function($scope) {
 	$scope.quote = 'Hello, World!';
 	$scope.author = 'Brian Kernighan';
+});
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/index.html',
+			controller: 'MainCtrl'
+		})
 });
