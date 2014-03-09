812c0c76f4c2469d5ad73678989c4bff45bdce89
David Minnerly <dtreeLive@hotmail.com>
2014-03-09 17:49:12 -0400

    Setup mechanism to determine the image to use via query string.

--- app/js/services/background.js
+++ app/js/services/background.js
@@ -1,11 +1,20 @@
 
 'use strict';
 
-app.factory('Background', function() {
+app.factory('Background', function($location) {
 	var Background = {
 		getFrom: function(json) {
-			var background = this.getRandom(json),
-				image = background.image,
+			var background;
+
+			// Check if a query string is set to determine the image to display,
+			// otherwise get a random one.
+			if (detectImageQuery()) {
+				background = this.getFromQuery(json);
+			} else {
+				background = this.getRandom(json);
+			}
+
+			var image = background.image,
 				imageList = background.imageList;
 
 			var imageData = {
@@ -40,8 +49,38 @@ app.factory('Background', function() {
 				image: randomImage,
 				imageList: randomImageListObj
 			};
+		},
+
+		getFromQuery: function(json) {
+			var backgroundArray = json.data.backgrounds,
+				query = $location.search(),
+				queriedImage,
+				queriedImageListObj;
+
+			if (query.bg) {
+				queriedImage = backgroundArray[query.bg];
+			}
+
+			if (query.bgSub) {
+				queriedImageListObj = queriedImage.image_list[query.bgSub];
+			} else {
+				queriedImageListObj = queriedImage.image_list[0];
+			}
+
+			return {
+				image: queriedImage,
+				imageList: queriedImageListObj
+			};
 		}
 	};
+
+	function detectImageQuery() {
+		var query = $location.search();
+
+		if ((query.bg) || (query.bg && query.bgSub)) {
+			return true;
+		}
+	}
 
 	function randomArrayIndex(array) {
 		return Math.floor(Math.random() * array.length);
