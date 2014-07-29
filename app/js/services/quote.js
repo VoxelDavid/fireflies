124f5db3fd56b9fb172fdb775dc32bd95734ea55
David Minnerly <dtreeLive@hotmail.com>
2014-07-29 15:41:07 -0400

    Wrap all of the functions in the factory definition.

--- app/js/services/quote.js
+++ app/js/services/quote.js
@@ -3,48 +3,48 @@
 
 var NO_AUTHOR = 'Unknown';
 
-function randomArrayIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-function randomQuote(json) {
-  var quoteData = json.data.quoteData,
-
-      i = randomArrayIndex(quoteData),
-      quoteMeta = quoteData[i],
-      quoteList = quoteMeta.quotes,
-
-      j = randomArrayIndex(quoteList),
-      quoteObject = quoteList[j];
-
-  return {
-    meta: quoteMeta,
-    data: quoteObject
-  };
-}
-
-function getQuoteData(json) {
-  var quote  = randomQuote(json),
-      meta   = quote.meta,
-      data   = quote.data,
-
-      author = meta.author,
-      title  = meta.title,
-      source = data.source,
-      text   = data.text;
-
-  if (!author) {
-    author = NO_AUTHOR;
+app.factory('Quote', function() {
+  function randomArrayIndex(array) {
+    return Math.floor(Math.random() * array.length);
   }
 
-  return {
-    author: author,
-    title: title,
-    source: source,
-    text: text
-  };
-}
+  function randomQuote(json) {
+    var quoteData = json.data.quoteData,
 
-app.factory('Quote', function() {
+        i = randomArrayIndex(quoteData),
+        quoteMeta = quoteData[i],
+        quoteList = quoteMeta.quotes,
+
+        j = randomArrayIndex(quoteList),
+        quoteObject = quoteList[j];
+
+    return {
+      meta: quoteMeta,
+      data: quoteObject
+    };
+  }
+
+  function getQuoteData(json) {
+    var quote  = randomQuote(json),
+        meta   = quote.meta,
+        data   = quote.data,
+
+        author = meta.author,
+        title  = meta.title,
+        source = data.source,
+        text   = data.text;
+
+    if (!author) {
+      author = NO_AUTHOR;
+    }
+
+    return {
+      author: author,
+      title: title,
+      source: source,
+      text: text
+    };
+  }
+
   return getQuoteData;
 });
