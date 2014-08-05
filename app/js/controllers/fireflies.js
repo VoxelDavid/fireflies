f1e384acb5521468e5e7603a2770d4835bff3fa1
David Minnerly <dtreeLive@hotmail.com>
2014-08-04 21:45:24 -0400

    Overhaul core app logic.

--- app/js/controllers/fireflies.js
+++ app/js/controllers/fireflies.js
@@ -1,22 +1,20 @@
 
 'use strict';
 
-app.controller('FirefliesCtrl', function($scope, data, background, Quote, Stellar) {
+app.controller('FirefliesCtrl', function($scope, Stellar, randomQuote, randomImage) {
   Stellar.init();
-
-  data.then(function(json) {
-    var quote = new Quote(json),
-        imageData = background.getFrom(json);
-
+  
+  randomImage.then(function(data) {
     // Apply the class of the chosen image to the scope.
-    $scope.backgroundClass = imageData.className;
-
-    // Quote and author are applied via the ng-bind-html directive, which uses
-    // $sanitize to preserve html tags and entities.
-    $scope.quote  = quote.text;
-    $scope.author = quote.author;
-    $scope.source = quote.source;
-    $scope.title  = quote.title;
+    $scope.backgroundClass = data.className;
+  });
+  
+  randomQuote.then(function(data) {
+    // The quote's author and text are set by the 'ng-bind-html' directive,
+    // which uses $sanitize to preserve html tags and entities.
+    $scope.quote  = data.text;
+    $scope.author = data.author;
+    $scope.source = data.source;
+    $scope.title  = data.title;
   });
 });
-
--- app/js/services/data.js
+++ app/js/services/data.js
@@ -1,6 +1,118 @@
 
 'use strict';
 
-app.factory('data', function($http) {
-  return $http.get('js/data.json');
+// The text displayed when there is no author set for the quote/background.
+var NO_AUTHOR = 'Unknown';
+
+var JSON_PATH = 'js/data.json';
+
+var random = Math.random,
+    floor  = Math.floor;
+
+var Data = {};
+
+
+/* Utilities
+============================================================================= */
+
+function randomIndex(array) {
+  var index = floor(random() * array.length);
+  return array[index];
+}
+
+function authorName(author) {
+  if (!author) {
+    return NO_AUTHOR;
+  } else {
+    return author;
+  }
+}
+
+function toplevel(obj, type) {
+  var meta = obj.meta,
+      data = obj.data;
+
+  function quote() {
+    return {
+      author: authorName(meta.author),
+      title: data.title,
+      source: data.source,
+      text: data.text
+    };
+  }
+
+  function image() {
+    return {
+      author: authorName(meta.author),
+      name: data.name,
+      className: data.className,
+      source: data.source
+    };
+  }
+
+  if (type === 'quote') {
+    return quote();
+  } else if (type === 'image') {
+    return image();
+  }
+}
+
+
+/* Data Methods
+============================================================================= */
+
+Data.randomObject = function(array, listName) {
+  var meta = randomIndex(array),
+      list = meta[listName],
+      data = randomIndex(list);
+  
+  return {
+    meta: meta,
+    data: data
+  };
+};
+
+Data.quote = function(json) {
+  var quoteData = json.quoteData,
+      randomQuoteData = this.randomObject(quoteData, 'quotes'),
+      quote = toplevel(randomQuoteData, 'quote');
+  
+  return quote;
+};
+
+Data.background = function(json) {
+  var imageData = json.imageData,
+      randomImageData = this.randomObject(imageData, 'images'),
+      image = toplevel(randomImageData, 'image');
+
+  return image;
+};
+
+
+/* Services & Factories
+============================================================================= */
+
+app.service('siteData', function($http) {
+//  return $http.get('js/data.json');
+  var data = $http.get(JSON_PATH).then(function(json) {
+    return json.data;
+  });
+
+  return data;
+});
+
+app.service('randomQuote', function(siteData) {
+  var quote = siteData.then(function(json) {
+    return Data.quote(json);
+  });
+  
+  return quote;
+});
+
+app.service('randomImage', function(siteData) {
+  var image = siteData.then(function(json) {
+    return Data.background(json);
+  });
+  
+  return image;
 });
