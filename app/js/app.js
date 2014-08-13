7f8dacfcb8271b9c7cba7b4598161e8266902582
David Minnerly <dtreeLive@hotmail.com>
2014-08-13 10:21:31 -0400

    Remove routing configuration.

--- app/js/app.js
+++ app/js/app.js
@@ -2,25 +2,7 @@
 'use strict';
 
 var app = angular.module('firefliesApp', [
-  'ngRoute',
   'ngSanitize'
 ]);
 
 app.constant('JSON_PATH', 'js/data.json');
-
-app.config(function($routeProvider, $locationProvider) {
-  $locationProvider
-    /* With this enabled, the url will appear as www.example.com/, without
-     * the #/ at the end. This is only used right now because there's only
-     * one view.
-     *
-     * Once there are more I believe rewrite rules need to be written, or
-     * manually typing a view will result in a 404. */
-    .html5Mode(true);
-
-  $routeProvider
-    .when('/', {
-      templateUrl: 'js/views/quote.html'
-    })
-    .otherwise({ redirectTo: '/' });
-});
