fe35f209fdc021eda249d4b6c9dc8212dcb137c6
David Minnerly <dtreeLive@hotmail.com>
2015-08-13 17:48:09 -0400

    Add back all the styling and markup
    Now Fireflies looks exactly like it did before everything was removed to
    convert over to Django.

---
+++ config.rb
@@ -0,0 +1,9 @@
+require "susy"
+
+project_path = "fireflies/static/fireflies"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+line_comments = false
