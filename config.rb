a961413111c68bd70a901fd9a0da10afbb85ee62
David Minnerly <dtreeLive@hotmail.com>
2015-08-14 12:37:22 -0400

    Add a note about what config.rb is

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
+# This is the config file for Compass, a Ruby based tool used for compiling Sass
+# files to CSS. If you have no intention of working with the CSS then you don't
+# need to worry about this.
+
 require "susy"
 
 project_path = "fireflies/static/fireflies"
