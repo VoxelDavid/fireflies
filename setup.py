0597bc0c836c0849b9c8b61c8ca8f1904c71aceb
David Minnerly <dtreeLive@hotmail.com>
2015-08-26 15:23:33 -0400

    Ensure all package data is installed

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ def readme():
 setup(
     name="fireflies",
     version="5.0.0",
-    packages=["fireflies"],
+    packages=[
+        "fireflies",
+        "fireflies.migrations"
+    ],
     description="A small application that displays a quote and image at random.",
     long_description=readme(),
     url="https://github.com/voxeldavid/fireflies",
     author="David Minnerly",
     license="MIT",
     keywords="fireflies background image quote web app django",
+    include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
